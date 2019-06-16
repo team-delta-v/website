6955e00ae45ec7a669b31b6a1e8538e6b47341c8
GloriousYellow <yellowglorious@gmail.com>
2019-06-16 10:46:56 -0400

    logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,31 +1,101 @@
-export default () => (
-  <div>
-    <div className="container">
-      <p className="text">ΔV</p>
-    </div>
-    <style jsx>
-      {`
-        .text {
-          color: orange;
+import React from 'react'
+import interpolate from 'color-interpolate'
+import io from 'socket.io-client'
+import { ClocksyClient } from 'clocksy'
 
-          font-size: 80vmin;
-          font-family: Jura, Roboto, sans-serif;
-          font-weight: 400;
+const colormap = interpolate([
+  'red',
+  'yellow',
+  'lime',
+  'aqua',
+  'blue',
+  'purple',
+  'red',
+])
 
-          text-align: center;
-          overflow: visible;
-          margin: 0;
+function reduce(time) {
+  return (time / 20000) % 1
+}
 
-          user-select: none;
-        }
+function getColor(time) {
+  const direction = (time / 15) % 360
+  const color1 = colormap(reduce(time - 4000))
+  const color2 = colormap(reduce(time))
+  return `linear-gradient(${direction}deg, ${color1}, ${color2})`
+}
 
-        .container {
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      `}
-    </style>
-  </div>
-)
+export default class extends React.Component {
+  state = { delta: 0 }
+
+  componentDidMount() {
+    this.animate()
+
+    this.socket = io('https://ntp.glitch.me/', { transports: ['websocket'] })
+    this.clocksy = new ClocksyClient({
+      sendRequest: data => {
+        this.socket.emit('t', data)
+      },
+      updatePeriod: 500,
+      alpha: 0.05,
+    })
+
+    this.socket.on('connect', () => this.clocksy.start())
+    this.socket.on('disconnect', () => this.clocksy.stop())
+
+    this.socket.on('t', data => {
+      const delta = this.clocksy.processResponse(data)
+      this.setState({ delta })
+    })
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.raf)
+    this.socket.close()
+    this.clocksy.stop()
+  }
+
+  animate = () => {
+    this.raf = requestAnimationFrame(() => {
+      const time = Date.now() + this.state.delta
+      this.setState({ color: getColor(time) })
+      this.animate()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <p className="text">ΔV</p>
+        </div>
+        <style jsx>
+          {`
+            .text {
+              background: ${this.state.color};
+
+              -webkit-background-clip: text;
+              -webkit-text-fill-color: transparent;
+
+              font-size: 80vmin;
+              font-family: Jura, Roboto, sans-serif;
+              font-weight: 400;
+
+              text-align: center;
+              overflow: visible;
+              margin: 0;
+
+              user-select: none;
+            }
+
+            .container {
+              height: 100vh;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
