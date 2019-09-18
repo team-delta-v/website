99b0dfb2bc2f406a4acebd205bec56c58d6d15e4
Dan <dan@yellowiki.xyz>
2019-09-18 16:21:31 -0400

    1

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,7 +7,10 @@ export default () => (
       spatialization and text-to-speech to help the visually impaired see the
       world with sound using only a smartphone and headphones.
     </p>
-    <p>Top 10 and 1st Place for Best Hack that Helps the Community</p>
+    <p>
+      Top 10 out of 240 college-level teams and 1st Place for Best Hack that
+      Helps the Community presented by CBRE
+    </p>
     <h3>News 451</h3>
     <p>
       News 451 is a minimalistic web application that allows users to see the
