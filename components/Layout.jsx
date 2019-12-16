3bea67c10c9095602ac15cb384e4d4ecf2250ff5
Dan <dan@yellowiki.xyz>
2019-12-16 14:26:26 -0500

    new icons

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -24,9 +24,33 @@ export default ({ children }) => {
           rel="stylesheet"
         />
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-touch-icon.png"
+        />
+        <link
           rel="icon"
           type="image/png"
-          href="/static/icons/icon-512x512.png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/icons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/static/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#292a36" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
         />
         <meta name="theme-color" content="#292a36" />
         <link rel="manifest" href="/static/manifest.json" />
